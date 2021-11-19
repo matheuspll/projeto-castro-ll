d50a31ce57f52e3a48de04e8897db2e4fe9a40af
Matheus Magalhães <matheus.magalha95@gmail.com>
2021-11-19 00:15:00 -0300

    Criando cadastro do blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
+from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 from .models import Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_cadastrar.html'
+    fields = '__all__'
+    success_url = reverse_lazy('home')
 
 
 class PostListView(ListView):
