1f76b4ad68b0b65fb14525067922c51977206d96
Matheus Magalhães <matheus.magalha95@gmail.com>
2021-11-25 21:42:29 -0300

    forms Contato

--- aplicacao/forms.py
+++ aplicacao/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 
 
+class ContatoForm(forms.Form):
+    nome = forms.CharField(max_length=100)
+    email = forms.CharField(max_length=100)
+    mensagem = forms.CharField(widget=forms.Textarea())
